c993d44bb36b2bbf2ab4bfa84c24b508a172c0f9
Jamin13P <benjaminthomsen1215@yahoo.com>
2020-05-11 15:05:50 -0700

    finished

--- classes.js
+++ classes.js
@@ -31,6 +31,18 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +60,19 @@
 */
 
 //Code Here
+
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(Employee) {
+    this.reports.push(Employee)
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +97,39 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age, reports)
+    this.title = "Not a manager"
+    this.bonus = 0
+  }
+  titleChange() {
+    let repNum = this.reports.length
+    if(repNum === 0){
+      this.title = "Not a manager"
+    } else if(repNum >= 1 && repNum <= 3){
+      this.title = "Barely Manager"
+    } else if(repNum >= 4 && repNum <= 10){
+      this.title = "Mostly Manager"
+    } else if(repNum >= 11 && repNum <= 50){
+      this.title = "Manager"
+    } else if(repNum >= 51 && repNum <= 100){
+      this.title = "Manager Plus"
+    } else if(repNum >= 101){
+      this.title = "Bestest Manager"
+    }
+  }
+  hire(Employee) {
+    super.hire()
+    this.titleChange()
+  }
+  fire(index) {
+    super.fire()
+    this.titleChange()
+    this.bonus += 100
+  }
+}
 
 
 
@@ -100,4 +158,24 @@
 
 //Code Here
 
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num
+    this.wear_and_tear_count = Math.floor(this.widgets_made_count / 50)
+  }
+  fixMachine(needs_reboot) {
+    this.needs_reboot = true
+  }
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
 
--- closures.js
+++ closures.js
@@ -24,11 +24,15 @@ function outer() {
   
 // Code Here
 
+const inner = outer()
+
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+
+inner()
 
 
 
@@ -53,6 +57,8 @@ function callFriend(name) {
 
 //Code Here
 
+const callJake = callFriend("Jake")
+
 
 
 ////////// PROBLEM 3 //////////
@@ -63,14 +69,21 @@ function callFriend(name) {
 
 //Code Here
 
+function makeCounter(){
+  let num = 0
+  return function(){
+    return ++num
+  }
+}
+
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -89,15 +102,22 @@ function counterFactory(value) {
   // Code here.
 
   return {
-
+    inc: function() {
+      value += 1
+      return value
+    },
+    dec: function() {
+      value -= 1
+      return value
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -114,8 +134,12 @@ function motivation( firstname, lastname ) {
 
   // code message function here.
 
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
+
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,8 +168,12 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function(){
+      return privateMethod()
+    }
   };
 })();
+module.publicMethod()
 
 
 
@@ -163,6 +191,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(add){
+     return secret += add
+    },
+    takeAwayFromSecret: function(sub){
+     return secret -= sub
+    }
   };
 }
 
@@ -188,8 +222,9 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    const time = i
     setTimeout(function() {
-      console.log(i);
+      console.log(time);
     }, i * 1000);
   }
 }
